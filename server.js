a6c32b93668ca795a6eb8b62846e7b5e3598c814
Christian Briscoe <cbriscoe1995@gmail.com>
2017-05-05 00:03:35 -0400

    Trying to dynamically generate page routes

--- server.js
+++ server.js
@@ -4,45 +4,68 @@ app.set('view engine', 'ejs')
 var port = process.env.PORT || 8080
 app.use(express.static('public'))
 
-app.get('/', function (request, response) {
-  response.render('pages/index', {
-    items: items
-  })
-})
-app.get('/barbarian', function (request, response) {
-  response.render('pages/barbarian')
-})
-app.get('/rogue', function (request, response) {
-  response.render('pages/rogue')
-})
-app.get('/sorcerer', function (request, response) {
-  response.render('pages/sorcerer')
-})
 var items = {}
-
+var allClasses = {}
 function createItem (item) {
   var id = Object.keys(items).length
   item.createdAt = new Date()
   items[id] = item
 }
+app.get('/', function (request, response) {
+  response.render('pages/index', {
+    items: items
+  })
+})
+// Object.keys(items).forEach(function (id) {
+//   var heroClass = items[id]
+//   app.get(heroClass.link, function (request, response) {
+//     response.render(heroClass.template, {
+//       allClasses: items,
+//       heroClass: heroClass
+//     })
+//     response.render(heroClass.template)
+//   })
+// })
+Object.keys(items).forEach(function (id) {
+  var heroClass = items[id]
+  app.get(items.link, function(request, response) {
+    reponse.render(items.template, {
+      allClasses: items,
+      heroClass: heroClass
+    })
+  })
+})
+
+// app.get('/barbarian', function (request, response) {
+//   response.render('pages/barbarian')
+// })
+// app.get('/rogue', function (request, response) {
+//   response.render('pages/rogue')
+// })
+// app.get('/sorcerer', function (request, response) {
+//   response.render('pages/sorcerer')
+// })
 
 createItem({
-  image: '/images/barbarian.tif',
-  link: 'barbarian',
+  template: 'pages/barbarian',
+  image: '/images/barbarian.jpg',
+  link: '/barbarian',
   title: 'Barbarian',
   source: 'Source: Core Player Handbook',
   archetype: 'Melee Striker'
 })
 createItem({
-  image: '/images/rogue.tif',
-  link: 'rogue',
+  template: 'pages/rogue',
+  image: '/images/rogue.jpg',
+  link: '/rogue',
   title: 'Rogue',
   source: 'Source: Core Player Handbook',
   archetype: 'Melee or Ranged Trickster'
 })
 createItem({
-  image: '/images/sorcerer.tif',
-  link: 'sorcerer',
+  template: 'pages/sorcerer',
+  image: '/images/sorcerer.jpg',
+  link: '/sorcerer',
   title: 'Sorcerer',
   source: 'Source: Core Player Handbook',
   archetype: 'Magical Blaster'
